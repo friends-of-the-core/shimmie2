5afef394e6d66c5bb700effedd7016c26b3161bb
Jessica Stokes <hello@jessicastokes.net>
2025-03-30 11:19:07 -0700

    filename_metadata: handle updated Discord username format

--- ext/filename_metadata/main.php
+++ ext/filename_metadata/main.php
@@ -9,7 +9,10 @@ class FilenameMetadata extends Extension
         return 40;
     }
 
-    public const FILENAME_REGEX = '/\[(?<username>\w+)-(?<date>[\w:\.\+-]+)\]-(?<discord_id>\w+)-(?<filename>.+)/';
+    // > [Discord usernames are now] limited to lowercase characters (a-z),
+    // > numbers (0-9) and two special characters (period and underscore)
+    // Source: https://discord.com/blog/usernames
+    public const FILENAME_REGEX = '/\[(?<username>[a-zA-Z0-9\._]+)-(?<date>[\w:\.\+-]+)\]-(?<discord_id>\w+)-(?<filename>.+)/';
 
     public function onImageAddition(ImageAdditionEvent $event)
     {
